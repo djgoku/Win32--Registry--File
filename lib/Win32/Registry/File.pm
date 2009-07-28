f485bff365abdfb76a8b9409537c1cc58b362d52
Jonathan C. Otsuka <djgoku@gmail.com>
2009-07-27 19:50:32 -0500

    File.pm: Added, this will be the main class which open/reads a window registry file MANIFEST: adding files needed for build Makefile.PL: Added.

---
+++ lib/Win32/Registry/File.pm
@@ -0,0 +1,107 @@
+package Win32::Registry::File;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Win32::Registry::File - The great new Win32::Registry::File!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Win32::Registry::File;
+
+    my $foo = Win32::Registry::File->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Jonathan C. Otsuka, C<< <djgoku at gmail.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-win32-registry-file at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Win32-Registry-File>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Win32::Registry::File
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Win32-Registry-File>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Win32-Registry-File>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Win32-Registry-File>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Win32-Registry-File/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Jonathan C. Otsuka, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+
+=cut
+
+1; # End of Win32::Registry::File
